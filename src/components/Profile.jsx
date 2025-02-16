03dd7f2f408e448054b189bd7623a235a533b472
mothibimalebana <mothibi.mm12@gmail.com>
2025-02-16 21:43:24 +0200

    created Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+    return(
+        <>
+        <div>
+            <h1>Hello from Profile Page</h1>
+            <p>So, how are you?</p>
+        </div>
+        </>
+    )
+}
+
+export default Profile
